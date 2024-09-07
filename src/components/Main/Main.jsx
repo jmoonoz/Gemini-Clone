b5f158d003791bac497aa20990562f32b40f732d
Joel Munoz <j.munoz1@yahoo.com>
2024-09-06 23:13:37 -0700

    main visual edits

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import "./Main.scss"
+import React from "react";
+import "./Main.scss";
+import { assets } from "../../assets/assets";
 
 const Main = () => {
   return (
-    <div>
-
+    <div className="main">
+      <div className="nav">
+        <p>Gemini</p>
+        <img src={assets.user_icon} alt="" />
+      </div>
+      <div className="main-container">
+        <div className="greet">
+          <p>
+            <span>Hello, Joel!</span>
+          </p>
+          <p>How can I help you today</p>
+        </div>
+        <div className="cards">
+          <div className="card">
+            <p>Such beautifull places to see on an upcoming trip</p>
+            <img src={assets.compass_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Breifly sumarize this concept: urban planing</p>
+            <img src={assets.bulb_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Brainstorm team bonding activities for the teams retreat</p>
+            <img src={assets.message_icon} alt="" />
+          </div>
+          <div className="card">
+            <p>Improve the readability for the following code</p>
+            <img src={assets.code_icon} alt="" />
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
