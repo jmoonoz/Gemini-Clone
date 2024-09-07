c10f7c94e316b0f0b58e3ecca64ce875c6bae1ca
Joel Munoz <j.munoz1@yahoo.com>
2024-09-06 19:59:04 -0700

    side bar created

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import Sidebar from "./components/Sidebar/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Sidebar />
+    </>
+  );
+}
+
+export default App;
